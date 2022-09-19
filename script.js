8db73d3d9b4a79aa8236eb51de457ef3df492eee
Kosoibrodjaga <113120088+Kosoibrodjaga@users.noreply.github.com>
2022-09-19 19:25:17 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function a() {
+    return 3 > 2
+}
+
+function b() {
+    return 0 > 1
+}
+
+function c() {
+    return 3 == 3
+}
+
+function d() {
+    return 0 >= 1
+}
+
+function e() {
+    return 0 <= 1
+}
+
+console.log (a(), b(), c(), d(), e())
+
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+let result = sum(3 , 4)
+
+console.log(result)
+
+
+function aaa() {
+    let aaa = 5
+    return aaa * 2
+    let result = aaa
+}
+
+console.log(aaa())
+
